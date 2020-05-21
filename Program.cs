69f722f7079dd11dc5c3577945cfd6452fd9fcb6
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2020-05-20 21:56:29 -0500

    detectFaces_method

--- Program.cs
+++ Program.cs
@@ -12,6 +12,9 @@ namespace face_quickstart
 {
     class Program
     {
+        // Used for all examples.
+        // URL for the images.
+        const string IMAGE_BASE_URL = "https://csdx.blob.core.windows.net/resources/Face/Images/";
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Adminde!");
@@ -19,6 +22,18 @@ namespace face_quickstart
             // Set your environment variables using the names below. Close and reopen your project for changes to take effect.
             string SUBSCRIPTION_KEY = Environment.GetEnvironmentVariable("FACE_SUBSCRIPTION_KEY");
             string ENDPOINT = Environment.GetEnvironmentVariable("FACE_ENDPOINT");
+
+            // Authenticate.
+            IFaceClient client = Authenticate(ENDPOINT, SUBSCRIPTION_KEY);
+
+            // Used in the Detect Faces and Verify examples.
+            // Recognition model 2 is used for feature extraction, use 1 to simply recognize/detect a face. 
+            // However, the API calls to Detection that are used with Verify, Find Similar, or Identify must share the same recognition model.
+            const string RECOGNITION_MODEL2 = RecognitionModel.Recognition02;
+            const string RECOGNITION_MODEL1 = RecognitionModel.Recognition01;
+
+            // Detect - get features from faces.
+            DetectFaceExtract(client, IMAGE_BASE_URL, RECOGNITION_MODEL2).Wait();
         }
         /*
         *	AUTHENTICATE
@@ -27,6 +42,42 @@ namespace face_quickstart
         public static IFaceClient Authenticate(string endpoint, string key)
         {
             return new FaceClient(new ApiKeyServiceClientCredentials(key)) { Endpoint = endpoint };
+        }
+
+        /* 
+        * DETECT FACES
+        * Detects features from faces and IDs them.
+        */
+        public static async Task DetectFaceExtract(IFaceClient client, string url, string recognitionModel)
+        {
+            Console.WriteLine("========DETECT FACES========");
+            Console.WriteLine();
+
+            // Create a list of images
+            List<string> imageFileNames = new List<string>
+                            {
+                                "detection1.jpg",    // single female with glasses
+                                // "detection2.jpg", // (optional: single man)
+                                // "detection3.jpg", // (optional: single male construction worker)
+                                // "detection4.jpg", // (optional: 3 people at cafe, 1 is blurred)
+                                "detection5.jpg",    // family, woman child man
+                                "detection6.jpg"     // elderly couple, male female
+                            };
+
+            foreach (var imageFileName in imageFileNames)
+            {
+                IList<DetectedFace> detectedFaces;
+
+                // Detect faces with all attributes from image url.
+                detectedFaces = await client.Face.DetectWithUrlAsync($"{url}{imageFileName}",
+                        returnFaceAttributes: new List<FaceAttributeType> { FaceAttributeType.Accessories, FaceAttributeType.Age,
+                        FaceAttributeType.Blur, FaceAttributeType.Emotion, FaceAttributeType.Exposure, FaceAttributeType.FacialHair,
+                        FaceAttributeType.Gender, FaceAttributeType.Glasses, FaceAttributeType.Hair, FaceAttributeType.HeadPose,
+                        FaceAttributeType.Makeup, FaceAttributeType.Noise, FaceAttributeType.Occlusion, FaceAttributeType.Smile },
+                        recognitionModel: recognitionModel);
+
+                Console.WriteLine($"{detectedFaces.Count} face(s) detected from image `{imageFileName}`.");
+            }
         }
     }
 }
